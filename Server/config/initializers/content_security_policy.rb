b53fb3622182b26d45b0e8fbf616bc02fd30976a
Polina Matskevich <polina199925@gmail.com>
2019-08-06 15:48:12 +0300

    Correct hound errors

--- Server/config/initializers/content_security_policy.rb
+++ Server/config/initializers/content_security_policy.rb
@@ -12,8 +12,8 @@
 #   policy.script_src  :self, :https
 #   policy.style_src   :self, :https
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
+# Specify URI for violation reports
+# policy.report_uri "/csp-violation-report-endpoint"
 # end
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request 
