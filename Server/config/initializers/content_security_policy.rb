c100566f44a282ad6df3b5af6fdf05a44a6ac7cd
Polina Matskevich <polina199925@gmail.com>
2019-08-06 13:06:22 +0300

    Fix whitespase errors

--- Server/config/initializers/content_security_policy.rb
+++ Server/config/initializers/content_security_policy.rb
@@ -15,11 +15,9 @@
 #   # Specify URI for violation reports
 #   # policy.report_uri "/csp-violation-report-endpoint"
 # end
-
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request 
 # { SecureRandom.base64(16) }
-
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
