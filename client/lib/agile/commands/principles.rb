6432923132bb545f80cc0840e11a431f7a0bcafd
qb <inudad@yandex.ru>
2019-08-07 20:17:30 +0300

    Fix principles methods

--- client/lib/agile/commands/principles.rb
+++ client/lib/agile/commands/principles.rb
@@ -2,35 +2,35 @@ module Agile
   class CLI < Thor
     desc "principles", "Shows twelwe agile principles"
     def principles
-      say "Our highest priority is to satisfy the customer\n
-through early and continuous delivery\n
-of valuable software.\n"
-      say "Welcome changing requirements, even late in\n
-development. Agile processes harness change for\n
-the customer's competitive advantage.\n"
-      say "Deliver working software frequently, from a \n
-couple of weeks to a couple of months, with a \n
-preference to the shorter timescale.\n"
-      say "Business people and developers must work\n
-together daily throughout the project.\n"
-      say "Build projects around motivated individuals.\n
-Give them the environment and support they need,\n
-and trust them to get the job done.\n"
-      say "The most efficient and effective method of\n
-conveying information to and within a development\n
-team is face-to-face conversation.\n"
-      say "Working software is the primary measure of progress.\n"
-      say "Agile processes promote sustainable development.\n
-The sponsors, developers, and users should be able\n
-to maintain a constant pace indefinitely.\n"
-      say "Continuous attention to technical excellence \n
-and good design enhances agility.\n"
-      say "Simplicity--the art of maximizing the amount \n
-of work not done--is essential.\n"
-      say "The best architectures, requirements, and designs \n
-emerge from self-organizing teams.\n"
-      say "At regular intervals, the team reflects on how \n
-to become more effective, then tunes and adjusts \n
+      say "Our highest priority is to satisfy the customer
+through early and continuous delivery
+of valuable software.
+      Welcome changing requirements, even late in
+development. Agile processes harness change for
+the customer's competitive advantage.
+      Deliver working software frequently, from a
+couple of weeks to a couple of months, with a
+preference to the shorter timescale.
+      Business people and developers must work
+together daily throughout the project.
+      Build projects around motivated individuals.
+Give them the environment and support they need,
+and trust them to get the job done.
+      The most efficient and effective method of
+conveying information to and within a development
+team is face-to-face conversation.
+      Working software is the primary measure of progress.
+      Agile processes promote sustainable development.
+The sponsors, developers, and users should be able
+to maintain a constant pace indefinitely.
+      Continuous attention to technical excellence
+and good design enhances agility.
+      Simplicity--the art of maximizing the amount
+of work not done--is essential.
+      The best architectures, requirements, and designs
+emerge from self-organizing teams.
+      At regular intervals, the team reflects on how
+to become more effective, then tunes and adjusts 
 its behavior accordingly.\n"
     end
   end
