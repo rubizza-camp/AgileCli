fd8ec3cfacbb364651c9b66777afddf245195228
qb <inudad@yandex.ru>
2019-08-07 13:56:36 +0300

    Refactor commands paths

--- client/lib/agile.rb
+++ client/lib/agile.rb
@@ -1,6 +1,6 @@
 require "thor"
 require "agile/constants"
-Dir[File.join(__dir__, 'agile/commands', '*.rb')].each { |file| require file }
+Dir[File.join(__dir__, "agile/commands", "*.rb")].each { |file| require file }
 
 module Agile
   class CLI < Thor
