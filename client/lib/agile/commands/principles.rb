c6ae1c506572822698d81ffdcbd9caf38bae550e
qb <inudad@yandex.ru>
2019-08-07 20:18:47 +0300

    Fix trailing whitespace

--- client/lib/agile/commands/principles.rb
+++ client/lib/agile/commands/principles.rb
@@ -30,7 +30,7 @@ of work not done--is essential.
       The best architectures, requirements, and designs
 emerge from self-organizing teams.
       At regular intervals, the team reflects on how
-to become more effective, then tunes and adjusts 
+to become more effective, then tunes and adjusts
 its behavior accordingly.\n"
     end
   end
