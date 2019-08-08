28075474de68b708c172d8c2c1c8a944c3de6ba6
qb <inudad@yandex.ru>
2019-08-08 12:47:44 +0300

    Fix version test

--- client/spec/agile/values_spec.rb
+++ client/spec/agile/values_spec.rb
@@ -1,7 +1,7 @@
 require "agile/commands/hello"
 
 RSpec.describe Agile::CLI do
-  describe "#hello" do
+  describe "#values" do
     subject(:cli) { described_class.new }
 
     before do
@@ -10,7 +10,6 @@ RSpec.describe Agile::CLI do
 
     it "has a version number" do
       cli.values
-
       expect(cli).to have_received(:say).with(File.read("lib/agile/agile_values.txt"))
     end
   end
