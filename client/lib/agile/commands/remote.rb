a1d6366d714a43de1abcdcaa10408b8aeb8e283c
epfeen <epfeen@gmail.com>
2019-08-12 15:56:17 +0300

    attr

--- client/lib/agile/commands/remote.rb
+++ client/lib/agile/commands/remote.rb
@@ -2,18 +2,24 @@ module Agile
   class Remote < Thor
     desc "add <url>", "Adds a remote"
     def add(url)
-      @remote = url
-      say "You remote : #{@remote}"
+      remote = url
+      say "You remote : #{remote}"
     end
 
     desc "drop", "Drops your remote"
     def drop
-      @remote = nil
+      remote = nil
       say "You droped remote with server"
     end
   end
 
   class CLI < Thor
+    attr_reader :remote
+
+    def initialize(remote)
+      @remote = remote
+    end
+
     desc Rainbow("remote SUBCOMMAND ...ARGS").cornflower, Rainbow("Work with remote").darkgoldenrod
     subcommand "remote", Remote
   end
