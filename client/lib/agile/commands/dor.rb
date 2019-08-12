9c14e160588f50f404f7ce755d9c3627aa7fd855
qb <inudad@yandex.ru>
2019-08-12 14:46:25 +0300

    Fix of dor command description

--- client/lib/agile/commands/dor.rb
+++ client/lib/agile/commands/dor.rb
@@ -1,6 +1,6 @@
 module Agile
   class CLI < Thor
-    desc Rainbow("dor").cornflower, Rainbow("Shows twelwe agile principles").darkgoldenrod
+    desc Rainbow("dor").cornflower, Rainbow("Shows you rules of writing items").darkgoldenrod
     def dor
       say File.read("#{`gem which agile`.chomp.chomp('agile.rb')}/agile/assets/definition_of_ready.txt")
     end
