e4720cb2de24b0e93e00b9776999980989e6e258
epfeen <epfeen@gmail.com>
2019-08-13 18:30:02 +0300

    fix hound

--- client/lib/agile/commands/projects.rb
+++ client/lib/agile/commands/projects.rb
@@ -6,23 +6,34 @@ module Agile
     subcommand "list", List
     subcommand "create", Create
     subcommand "use", Use
+    subcommand "invite", Invite
   end
 
   class Use < Thor
-    def use
-      #some code
+    desc "use <project>", "Select current project"
+    def use(project)
+      # some code
     end
   end
 
   class List < Thor
+    desc "list", "Show projects"
     def list
-      #some code
+      # some code
     end
   end
 
   class Create < Thor
-    def create
-      #some code
+    desc "create <project>", "Create new project"
+    def create(project)
+      # some code
+    end
+  end
+
+  class Invite < Thor
+    desc "invite <github>", "Add user to your project"
+    def invite(github)
+      # some code
     end
   end
 end
