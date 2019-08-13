04b5b0dcb021150c4506ec316de5b9ea87d8e009
epfeen <epfeen@gmail.com>
2019-08-13 18:17:43 +0300

    create classes

--- client/lib/agile/commands/projects.rb
+++ client/lib/agile/commands/projects.rb
@@ -1,8 +1,20 @@
 module Agile
   class CLI < Thor
     desc Rainbow("projects SUBCOMMAND ...ARGS").cornflower, Rainbow("Command for work with projects").darkgoldenrod
-    subcommand "list", #укажи класс
-    subcommand "create", #укажи класс
-    subcommand "use" #укажи класс
+    subcommand "list", List
+    subcommand "create", Create
+    subcommand "use", Use
+  end
+
+  class Use < Thor
+    #some code
+  end
+
+  class List < Thor
+    #some code
+  end
+
+  class Create < Thor
+    #some code
   end
 end
