20a0d6240827455a357f02f4f9a93e0d9d7a19ba
Polina Matskevich <polina199925@gmail.com>
2019-08-15 14:02:56 +0300

    Add restclient

--- client/agile-cli.gemspec
+++ client/agile-cli.gemspec
@@ -14,6 +14,7 @@ Gem::Specification.new do |spec|
   spec.executables   = ["agile"]
 
   spec.add_dependency "rainbow", "~> 3.0.0"
+  spec.add_dependency "rest-client", "~> 2.0.2"
   spec.add_dependency "thor", "~> 0.20.3"
 
   spec.add_development_dependency "bundler", "~> 2.0.2"
--- client/lib/agile/commands/login.rb
+++ client/lib/agile/commands/login.rb
@@ -1,14 +1,17 @@
 require "json"
+require "rest-client"
 
 module Agile
   class CLI < Thor
     include Thor::Actions
     desc Rainbow("login LOGIN_FROM_GITHUB").cornflower, Rainbow("Sign in github.").darkgoldenrod
+  
     def login(username)
       create_config_file
       config = read_config_content
       config["current_user"] = github_user_login(username)
       File.write("#{GEM_PATH}.config.json", JSON.generate(config))
+      authorize(config["current_user"])
     end
 
     private
@@ -27,6 +30,13 @@ module Agile
       else
         JSON.parse(File.read("#{GEM_PATH}.config.json"))
       end
+    end
+
+    def authorize(curr_user)
+      JSON.parse(`curl -s https://agile-cli.herokuapp.com/api/v1/users/#{curr_user}"`)["github_login"]
+     # r = RestClient.get "https://agile-cli.herokuapp.com/api/v1/users/#{curr_user}"
+     # say r.body
+      puts "hello"
     end
   end
 end
