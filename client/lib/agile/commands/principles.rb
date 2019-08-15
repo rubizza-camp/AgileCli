1a53f9cce97385b06a6cdfd82345b16ce8d3d68e
Andrew Berendeev <soligorsk162@gmail.com>
2019-08-15 17:18:02 +0300

    Add GEM_PATH in duplicate code and add homepage in help description

--- client/lib/agile/commands/principles.rb
+++ client/lib/agile/commands/principles.rb
@@ -3,7 +3,7 @@ module Agile
     desc Rainbow("principles").cornflower, Rainbow("Shows twelwe agile principles").darkgoldenrod
     def principles
       principl = []
-      principl << File.read("#{`gem which agile`.chomp.chomp('agile.rb')}/agile/assets/agile_principles.txt")
+      principl << File.read("#{GEM_PATH}/agile/assets/agile_principles.txt")
       say Terminal::Table.new title: "Agile principles", rows: [principl], style: TERMINAL_STYLE
     end
   end
