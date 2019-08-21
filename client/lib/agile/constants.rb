7a82d9dbd41dbf13557594811f788fbe857f41fd
Polina Matskevich <polina199925@gmail.com>
2019-08-21 10:13:51 +0300

    Fix

--- client/lib/agile/constants.rb
+++ client/lib/agile/constants.rb
@@ -5,7 +5,7 @@ module Agile
   VERSION = "0.0.11"
   GITHUB_URL = %(https://api.github.com/user)
   TERMINAL_STYLE = { border_x: "=", border_i: "x", padding_left: 3 }.freeze
-  URL_PATTERN = /[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$}/
-  LOCALHOST_PATTERN = /localhost:/
+  URL_PATTERN = /[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$}/.freeze
+  LOCALHOST_PATTERN = /localhost:/.freeze
 end
 # rubocop:enable Style/RegexpLiteral
