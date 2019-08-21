aef0faf20fdfcbfd53ed67e4c7e3d194448da48f
Polina Matskevich <polina199925@gmail.com>
2019-08-21 10:09:11 +0300

    Regexp

--- client/lib/agile/constants.rb
+++ client/lib/agile/constants.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
-
+# rubocop:disable Style/RegexpLiteral
 module Agile
   VERSION = "0.0.11"
   GITHUB_URL = %(https://api.github.com/user)
   TERMINAL_STYLE = { border_x: "=", border_i: "x", padding_left: 3 }.freeze
-  URL_PATTERN = %r{[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$}
-  LOCALHOST_PATTERN = %r{localhost:}
+  URL_PATTERN = /[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$}/
+  LOCALHOST_PATTERN = /localhost:/
 end
+# rubocop:enable Style/RegexpLiteral
