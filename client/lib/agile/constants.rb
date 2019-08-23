6f6265245ca11ea3abe74d4843d644d1f42d2d4c
Polina Matskevich <polina199925@gmail.com>
2019-08-23 21:29:46 +0300

    Add spaces

--- client/lib/agile/constants.rb
+++ client/lib/agile/constants.rb
@@ -4,7 +4,7 @@ module Agile
   VERSION = "0.0.11"
   GITHUB_URL = %(https://github.com/login)
   TERMINAL_STYLE = { border_x: "=", border_i: "x", padding_left: 3 }.freeze
-  CLIENT_ID="8bf42da7e4f9032ac5d8"
-  CLIENT_SECRET="cfef987b3839a1a846ba3f0707aa036240d2d625"
+  CLIENT_ID = "8bf42da7e4f9032ac5d8"
+  CLIENT_SECRET = "cfef987b3839a1a846ba3f0707aa036240d2d625"
 end
 # rubocop:enable Style/RegexpLiteral
