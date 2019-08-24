4673af9be316a7fd4fcab27ee3152d78a6db22be
Polina Matskevich <polina199925@gmail.com>
2019-08-24 17:40:38 +0300

    Fix hound

--- client/lib/agile/commands/projects.rb
+++ client/lib/agile/commands/projects.rb
@@ -5,8 +5,8 @@ module Agile
     def create(project_name)
       error_checking
       response = RestClient.post "#{CONFIG['current_remote']}/api/v1/projects/",
-                 name: project_name,
-                 current_user: CONFIG["current_user"]
+                                 name: project_name,
+                                 current_user: CONFIG["current_user"]
       if response.body
         say "Successfully created project #{project_name}"
       else
