09b6d533dc34ca37fc1be2aeddf240c9b5660fee
epfeen <epfeen@gmail.com>
2019-08-26 16:17:54 +0300

    fix hound

--- client/lib/agile/commands/projects.rb
+++ client/lib/agile/commands/projects.rb
@@ -51,17 +51,21 @@ module Agile
     end
 
     def project_search(response, project)
-      info = JSON.parse(response).map { |hash| hash.values }
+      info = JSON.parse(response).map(&:values)
       id_pr = info.map { |arr| arr[0] if arr[1] == project }
-      id_pr.delete_if{ |proj| proj == nil }
+      id_pr.delete_if(&:nil?)
       if info[1].include?(project)
-        CONFIG["current_project"] = project
-        CONFIG["current_project_id"] = id_pr.first
-        File.write("#{GEM_PATH}.config.json", JSON.generate(CONFIG))
+        write_to_config(id_pr, project)
         say "Your project: #{project}"
       else
         say "Such project does not exist. Try again"
       end
+    end
+
+    def write_to_config(id_pr, project)
+      CONFIG["current_project_id"] = id_pr.first
+      CONFIG["current_project"] = project
+      File.write("#{GEM_PATH}.config.json", JSON.generate(CONFIG))
     end
   end
 
--- client/lib/agile/commands/tickets.rb
+++ client/lib/agile/commands/tickets.rb
@@ -1,12 +1,12 @@
 module Agile
   class Tickets < Thor
-    desc "take <ticket>", "Take ticket"
-    def take(ticket)
-      # call api to take ticket
-      CONFIG["tickets"].push(ticket)
-      File.write("#{GEM_PATH}.config.json", JSON.generate(CONFIG))
-      puts "you take ticket"
-    end
+    # desc "take <ticket>", "Take ticket"
+    # def take(ticket)
+    #   # call api to take ticket
+    #   CONFIG["tickets"].push(ticket)
+    #   File.write("#{GEM_PATH}.config.json", JSON.generate(CONFIG))
+    #   puts "you take ticket"
+    # end
 
     desc "create <ticket>", "Add new ticket"
     def create(ticket)
@@ -14,7 +14,8 @@ module Agile
       cli = HighLine.new
       ticket_description = cli.ask("description for ticket: ", String)
       RestClient.post"#{CONFIG['current_remote']}/api/v1/tickets/",
-                     project_id: CONFIG['current_project_id'], name: ticket_name, user: CONFIG['current_user'], desc: ticket_description
+                     project_id: CONFIG["current_project_id"], name: ticket_name,
+                     user: CONFIG["current_user"], desc: ticket_description
       say "Successfully added new ticket!"
     end
 
@@ -26,63 +27,55 @@ module Agile
       end
     end
 
-    desc "check <name_ticket>", "Check ticket"
-    def check(ticket)
-
-    end
-
-    desc "my_list", "Your tickets list"
-    def my_list
-      CONFIG["tickets"].each do |name|
-        if name == CONFIG["tickets"]
-          say "* #{name}"
-        else
-          say name
-        end
-      end
-    end
-
-    desc "update <ticket>", "update ticket"
-    def update(ticket)
-      choice = HighLine.new
-      choice.ask("Choose what you need to edit : name or description (N or D)", String)
-      if choice == "N"
-        # call api for name
-      elsif choice == "D"
-        # call api for description
-      else
-        say "Try again"
-      end
-    end
-
-    desc "delete <ticket>", "Delete ticket"
-    def delete(ticket)
-      # call api to delete ticket
-      puts "you delete ticket"
-    end
+  #   desc "check <name_ticket>", "Check ticket"
+  #   def check(ticket)
+  #   end
+  #
+  #   desc "my_list", "Your tickets list"
+  #   def my_list
+  #     CONFIG["tickets"].each do |name|
+  #       if name == CONFIG["tickets"]
+  #         say "* #{name}"
+  #       else
+  #         say name
+  #       end
+  #     end
+  #   end
+  #
+  #   desc "update <ticket>", "update ticket"
+  #   def update(ticket)
+  #     choice = HighLine.new
+  #     choice.ask("Choose what you need to edit : name or description (N or D)", String)
+  #     if choice == "N"
+  #       # call api for name
+  #     elsif choice == "D"
+  #       # call api for description
+  #     else
+  #       say "Try again"
+  #     end
+  #   end
+  #
+  #   desc "delete <ticket>", "Delete ticket"
+  #   def delete(ticket)
+  #     # call api to delete ticket
+  #     puts "you delete ticket"
+  #   end
   end
-
-  private
-
-  def cli_for_description
-    cli = HighLine.new
-    cli.ask("description for ticket: ", String)
-  end
-
-  def cli_for_change
-    cli = HighLine.new
-    cli.ask("name or description change? : ", String)
-  end
-
-  def call_cli_name
-    cli = HighLine.new
-    cli.ask("write new name : ", String)
-  end
-
-  def call_cli_desc
-    cli = HighLine.new
-    cli.ask("write new description : ", String)
-  end
+  # private
+  # def cli_for_description
+  #   cli = HighLine.new
+  #   cli.ask("description for ticket: ", String)
+  # end
+  #
+  # def cli_for_change
+  #   cli = HighLine.new
+  #   cli.ask("name or description change? : ", String)
+  # end
+  #
+  # def call_cli_name
+  #   cli = HighLine.new
+  #   cli.ask("write new name : ", String)
+  # end
 
   class CLI < Thor
     desc Rainbow("tickets SUBCOMMAND ...ARGS").cornflower, Rainbow("Command for work with tickets").darkgoldenrod
