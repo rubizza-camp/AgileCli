1ae32bb5157837e054d627774ade808d9188f893
epfeen <epfeen@gmail.com>
2019-08-28 15:25:50 +0300

    command events

--- client/lib/agile/commands/events.rb
+++ client/lib/agile/commands/events.rb
@@ -20,7 +20,19 @@ module Agile
       end
     end
 
+    desc "show <date (yyyy-mm-dd)>", "Show event"
+    def show(date)
+      response = RestClient.get "#{CONFIG['current_remote']}/api/v1/events/"
+      JSON.parse(response).each { |event| puts_info(event) if event["date"] == date }
+    end
+
     private
+
+    def puts_info(event)
+      say "Type of event: #{event['event_type']}\nDescription: #{event['description']}"
+      say "Start at #{norm_time(event['start_time'])}\nEnd at #{norm_time(event['end_time'])}"
+      puts ""
+    end
 
     def parse_info(event)
       { start: norm_time(event["start_time"]),
