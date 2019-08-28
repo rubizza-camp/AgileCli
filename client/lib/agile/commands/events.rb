86485677f8d6464921d31b7f963caaab3eed7cb2
epfeen <epfeen@gmail.com>
2019-08-28 12:10:20 +0300

    events list

--- client/lib/agile/commands/events.rb
+++ client/lib/agile/commands/events.rb
@@ -1,3 +1,6 @@
+require "date"
+require "time"
+
 module Agile
   class Events < Thor
     desc "create", "Add new event"
@@ -5,16 +8,28 @@ module Agile
       cli = HighLine.new
       event_description = cli.ask("description for event: ", String)
       RestClient.post"#{CONFIG['current_remote']}/api/v1/events/",
-                     event_type: type_cli, desc: event_description, frequency: frequency_cli,
-                     date: date_cli, time: time_cli, current_user: CONFIG["current_user"]
+                     event_type: type_cli, frequency: frequency_cli, date: date_cli,
+                     start_time: start_time_cli, end_time: end_time_cli, desc: event_description,
+                     current_user: CONFIG["current_user"]
       say "Successfully added new event!"
+    end
+
+    desc "list", "Show all events"
+    def list
+      response = RestClient.get "#{CONFIG['current_remote']}/api/v1/events/"
+      require 'pry';binding.pry
+      JSON.parse(response).each do |event|
+        d = DateTime.new(event["date"])
+        puts d
+        #puts "#{event["event_type"]} starting #{event["date"]}#{event["start_time"]} end at #{event["end_time"]}"
+      end
     end
 
     private
 
     def type_cli
       cli = HighLine.new
-      puts "0 - sprint\n1 - scrum\n2 - retro\n3 - planning\n4 - review"
+      puts "0 - scrum\n1 - retro\n2 - planning\n3 - review"
       cli.ask("Choose type of event (select number): ", Integer)
     end
 
@@ -29,9 +44,14 @@ module Agile
       cli.ask("Select data for event (yyyy.mm.dd): ", String)
     end
 
-    def time_cli
+    def start_time_cli
       cli = HighLine.new
-      cli.ask("Select time for event (hh:mm): ", String)
+      cli.ask("Select start time for event (hh:mm): ", String)
+    end
+
+    def end_time_cli
+      cli = HighLine.new
+      cli.ask("Select end time for event (hh:mm): ", String)
     end
   end
 
